9852e6b9096c6566baa443ddd47cc06e63f59e92
alisondraV <alice.7414122013@gmail.com>
2021-09-18 16:02:36 -0400

    Add Auth guard and navigation

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nosok/auth_guard.dart';
 import 'package:nosok/sign_in.dart';
+
 import 'home.dart';
 
 Future<void> main() async {
