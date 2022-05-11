336aeab92cb24116c87c4cdc58af530b978bded3
alisondraV <alice.7414122013@gmail.com>
2022-05-11 16:27:56 -0400

    Add sign in popup

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -28,12 +28,90 @@ class SignIn extends StatelessWidget {
         Container(
           margin: const EdgeInsets.all(20),
           child: ElevatedButton(
-            onPressed: () => signIn(context),
-            child: Text('Sign In'),
-            style: ElevatedButton.styleFrom(
-              minimumSize: const Size.fromHeight(48),
-            ),
-          ),
+              child: Text('Sign In'),
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(48),
+              ),
+              onPressed: () {
+                showModalBottomSheet<void>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Container(
+                      height: 800,
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            SizedBox(height: 30),
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                'Welcome!',
+                                style: Theme.of(context).textTheme.headline1,
+                              ),
+                            ),
+                            Text("Let's find a perfect pair for you sock :)"),
+                            TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Email Address',
+                              ),
+                            ),
+                            TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Password',
+                              ),
+                            ),
+                            ElevatedButton(
+                              child: const Text('Sign In'),
+                              onPressed: () => Navigator.pop(context),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 20),
+                              child: Row(
+                                children: <Widget>[
+                                  Expanded(child: Divider(height: 2)),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 10.0,
+                                    ),
+                                    child: Text(
+                                      'OR',
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(child: Divider(height: 2)),
+                                ],
+                              ),
+                            ),
+                            ElevatedButton(
+                              child: const Text('Continue with Google'),
+                              onPressed: () => signIn(context),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text("Don't have an account yet?"),
+                                Text(
+                                  ' Sign Up',
+                                  style: TextStyle(color: primaryColor),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }),
         ),
         TextButton(
           onPressed: () => signIn(context),
